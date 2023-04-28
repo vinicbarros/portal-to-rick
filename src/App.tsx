92a3de655b56da77a2f2071602543ff3b86a9a51
Vinícius Barros <vicaetano_barros@hotmail.com>
2023-04-28 00:23:03 -0300

    feat: created apollo client and provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { ApolloProvider } from "@apollo/client";
+import { client } from "./config/client";
+import Home from "./pages/Home";
+import { GlobalStyle } from "./style/GlobalStyle";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <GlobalStyle />
+      <ApolloProvider client={client}>
+        <Home />
+      </ApolloProvider>
+    </>
+  );
 }
 
 export default App;
