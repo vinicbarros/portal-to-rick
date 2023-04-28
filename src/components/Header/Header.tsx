109282c9d551f618301bba6222485bca7a21f44c
Vinícius Barros <vicaetano_barros@hotmail.com>
2023-04-28 09:10:25 -0300

    feat: added background image to header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import { SearchBar } from "../SearchBar/SearchBar";
 import FilterBy from "./FilterBy";
+import Portal from "../../assets/images/Portal.png";
 
 export default function Header() {
   return (
@@ -16,11 +17,17 @@ export default function Header() {
 
 const HeaderContent = styled.header`
   background-color: #5cad4a;
+  background-image: url(${Portal});
+  background-size: 500px;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-position-y: 180px;
   height: 400px;
   margin-bottom: 50px;
   display: flex;
   flex-direction: column;
   position: relative;
+  box-shadow: rgba(0, 0, 0, 0.15) 0px 15px 25px, rgba(0, 0, 0, 0.05) 0px 5px 10px;
 `;
 
 const Title = styled.h1`
@@ -30,6 +37,9 @@ const Title = styled.h1`
   margin-top: 60px;
   margin-bottom: 50px;
   align-self: center;
+  text-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px,
+    rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px,
+    rgba(0, 0, 0, 0.09) 0px -3px 5px;
 `;
 
 const FilterWrap = styled.div`
