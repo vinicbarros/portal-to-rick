8672204342850a3a027f7eab0e09c173fdaf2827
Vinícius Barros <vicaetano_barros@hotmail.com>
2023-04-28 08:45:21 -0300

    feat: add provider to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import { ApolloProvider } from "@apollo/client";
 import { client } from "./config/client";
+import { InfosContextProvider } from "./contexts/InfosContext";
 import Home from "./pages/Home";
 import { GlobalStyle } from "./style/GlobalStyle";
 
 function App() {
   return (
-    <>
+    <InfosContextProvider>
       <GlobalStyle />
       <ApolloProvider client={client}>
         <Home />
       </ApolloProvider>
-    </>
+    </InfosContextProvider>
   );
 }
 
